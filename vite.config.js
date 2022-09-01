81c8e6a25567258cc9e279b2cf1db523070d621d
davay <fond@fondfrukt.ru>
2022-09-01 23:30:39 +0000

    relative base

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),
+  plugins: [
+  vue(),
   WindiCSS({
     scan: {
       dirs: ['./src'],
@@ -20,8 +21,11 @@ export default defineConfig({
       '#': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  server: {
+    base:"./"
+  },
   build: {
-
+  
     chunkSizeWarningLimit: 100000,
     rollupOptions: {
       output: {
